7f855a817c1b4f269fdabf65cb87b8696586e5b5
balroop <balroop.thiar@capita.com>
2022-02-04 15:00:45 +0000

    second commit

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -48,7 +48,6 @@ function FeedbackForm() {
             } else {
                 addFeedback(newFeedback)
             }
-
             setText('')
         }
     }
--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,26 +1,26 @@
 import { motion, AnimatePresence } from 'framer-motion'
-import {useContext} from 'react'
+import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
+import Spinner from './shared/Spinner'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
+    const { feedback, isLoading } = useContext(FeedbackContext)
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback Yet</p>
     }
 
     //npm i framer-motion@4.1.17
-    return (
+    return isLoading ? (<Spinner />) : (
         <div className='feedback-list'>
             <AnimatePresence>
                 {feedback.map((item) => (
-                    <motion.div 
+                    <motion.div
                         key={item.id}
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity: 0}}
-                    >
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}>
                         <FeedbackItem
                             key={item.id}
                             item={item} />
